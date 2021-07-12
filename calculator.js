bd5374bb55731c5a333a8ec703228d95a1ae4ff5
Sajal Gupta <guptasajal411@gmail.com>
2021-07-12 20:52:01 +0530

    Add server side code for BMI

--- calculator.js
+++ calculator.js
@@ -15,6 +15,16 @@ app.post("/", function(req, res){
     res.send("Thanks for using this calculator!!<br>" + n1 + " + " + n2 + " = " + (n1 + n2));
 })
 
+app.get("/bmi", function(req, res){
+    res.sendFile(__dirname + "/bmi.html")
+})
+
+app.post("/bmi", function(req, res){
+    var height = Number(req.body.height);
+    var weight = Number(req.body.weight);
+    res.send("You BMI is: " + weight/(height * height));
+})
+
 app.listen(3000, function(){
     console.log("listening to port 3000");
 })
