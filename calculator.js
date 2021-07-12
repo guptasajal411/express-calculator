d6841b3a53287b9f6243d6e325cace271aed18c1
Sajal Gupta <guptasajal411@gmail.com>
2021-07-12 21:04:00 +0530

    Add connector buttons in the results page

--- calculator.js
+++ calculator.js
@@ -12,7 +12,8 @@ app.get("/", function(req, res){
 app.post("/", function(req, res){
     var n1 = Number(req.body.num1);
     var n2 = Number(req.body.num2);
-    res.send("Thanks for using this calculator!!<br>" + n1 + " + " + n2 + " = " + (n1 + n2));
+    res.send("Thanks for using this calculator!!<br>" + n1 + " + " + n2 + " = " + (n1 + n2) + '<br><a href="/"><button type="button">Add another numbers!</button></a><br><a href="/bmi"><button type="button">Try BMI!</button></a>'
+    );
 })
 
 app.get("/bmi", function(req, res){
@@ -22,7 +23,7 @@ app.get("/bmi", function(req, res){
 app.post("/bmi", function(req, res){
     var height = Number(req.body.height);
     var weight = Number(req.body.weight);
-    res.send("You BMI is: " + weight/(height * height));
+    res.send("You BMI is: " + weight/(height * height) + '<br><a href="/bmi"><button type="button">Calculate BMI again!</button></a><br><a href="/"><button type="button">Try addition!</button></a>');
 })
 
 app.listen(3000, function(){
